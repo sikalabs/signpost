e4b0f48701d2c864ff4da7e0cee387bc7fcec41b
Ondrej Sika <ondrej@ondrejsika.com>
2023-02-06 00:01:35 +0100

    feat(pkg/server): Add support for static files in /static

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -147,5 +147,7 @@ func Server(config Config) error {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, HTML)
 	})
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	return http.ListenAndServe(":8000", nil)
 }
